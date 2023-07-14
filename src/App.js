9d6f1e829cb4cfb4bda5cf8efa6fa99b1021b64c
fireguru <agururaja@google.com>
2023-07-13 22:22:23 -0400

    Translate functionality and final touches

--- src/App.js
+++ src/App.js
@@ -1,74 +1,143 @@
-import React, { useRef, useState } from 'react';
-import './App.css';
+import React, { useRef, useState, useEffect } from "react";
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import 'firebase/analytics';
+import "./App.css";
+import { setConfig } from "react-google-translate";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/analytics";
 
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
+//import { signOut } from "firebase/auth";
+
+// Setting up Google Cloud API Services
+//setConfig({
+//  clientEmail: "fireguru@fireguru-chatapp-392406.iam.gserviceaccount.com",
+//  privateKey: "AIzaSyDbb_O6zywfCxjIb_ajPtY9WagdNxjbwsM",
+//  projectId: "fireguru-chatapp-392406",
+//});
+
+//Initializing Firebase Application from Firebase
 firebase.initializeApp({
-  // your config
-})
+  apiKey: "AIzaSyBNhEBVc5MzZnsqlAKdCtesoYfeCXH8pSc",
+  authDomain: "fireguru-chatapp.firebaseapp.com",
+  projectId: "fireguru-chatapp",
+  storageBucket: "fireguru-chatapp.appspot.com",
+  messagingSenderId: "618129494494",
+  appId: "1:618129494494:web:2f8f0df376ff146e138d4f",
+  measurementId: "G-PWBBXH1TMQ",
+});
+
+const analyticsMock = {
+  logEvent: () => {},
+  setCurrentScreen: () => {},
+  setUserId: () => {},
+};
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-const analytics = firebase.analytics();
-
+export const analytics = firebase.analytics.isSupported()
+  ? firebase.analytics()
+  : analyticsMock;
 
 function App() {
-
   const [user] = useAuthState(auth);
+  const [selectedlanguage, setlanguage] = useState("en");
+  console.log(selectedlanguage);
 
   return (
     <div className="App">
-      <header>
-        <h1>⚛️🔥💬</h1>
-        <SignOut />
+      <header className="App-header">
+        <div>
+          {user ? (
+            <SignOut selectedlanguage={selectedlanguage} />
+          ) : (
+            <div> {user}</div>
+          )}
+        </div>
+        <div>
+          {user ? (
+            <Translate
+              selectedlanguage={selectedlanguage}
+              setlanguage={setlanguage}
+            />
+          ) : (
+            <div> {user}</div>
+          )}
+        </div>
       </header>
-
       <section>
-        {user ? <ChatRoom /> : <SignIn />}
+        {user ? <ChatRoom selectedlanguage={selectedlanguage} /> : <SignIn />}
       </section>
-
     </div>
   );
 }
 
-function SignIn() {
+function Translate(props) {
+  //  console.log(props);
+  const availablelanguages = ["de", "en", "es", "fr"];
+  return (
+    <select
+      value={props.selectedlanguage}
+      onChange={(e) => props.setlanguage(e.target.value)}
+      name="language"
+      id="language-select"
+    >
+      {availablelanguages.map((languagecode) => {
+        return (
+          <option key={languagecode} value={languagecode}>
+            {languagecode}
+          </option>
+        );
+      })}
+    </select>
+  );
+}
 
+function SignIn() {
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
-  }
+  };
 
   return (
     <>
-      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
+      <button className="sign-in" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+      <p>
+        Do not violate the community guidelines or you will be banned for life!
+      </p>
     </>
-  )
-
+  );
 }
 
-function SignOut() {
-  return auth.currentUser && (
-    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
-  )
+function SignOut(props) {
+  const signOutTranslations = {
+    de: "austragen",
+    en: "sign out",
+    es: "desconectar",
+    fr: "se déconnecter",
+  };
+
+  return (
+    auth.currentUser && (
+      <button className="sign-out" onClick={() => auth.signOut()}>
+        {signOutTranslations[props.selectedlanguage]}
+      </button>
+    )
+  );
 }
 
-
-function ChatRoom() {
+function ChatRoom(props) {
+  console.log("chatroom props", props);
   const dummy = useRef();
-  const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
-
-  const [messages] = useCollectionData(query, { idField: 'id' });
-
-  const [formValue, setFormValue] = useState('');
-
+  const messagesRef = firestore.collection("messages");
+  const query = messagesRef.orderBy("createdAt").limit(25);
+  const [messages] = useCollectionData(query, { idField: "id" });
+  const [formValue, setFormValue] = useState("");
 
   const sendMessage = async (e) => {
     e.preventDefault();
@@ -79,45 +148,68 @@ function ChatRoom() {
       text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid,
-      photoURL
-    })
+      photoURL,
+    });
 
-    setFormValue('');
-    dummy.current.scrollIntoView({ behavior: 'smooth' });
-  }
+    setFormValue("");
+    dummy.current.scrollIntoView({ behavior: "smooth" });
+  };
 
-  return (<>
-    <main>
+  const inputTranslations = {
+    de: "Sag etwas Nettes",
+    en: "say something nice",
+    es: "di algo agradable",
+    fr: "dis quelque chose de gentil",
+  };
 
-      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => (
+            <ChatMessage
+              selectedlanguage={props.selectedlanguage}
+              key={msg.id}
+              message={msg}
+            />
+          ))}
 
-      <span ref={dummy}></span>
+        <span ref={dummy}></span>
+      </main>
 
-    </main>
-
-    <form onSubmit={sendMessage}>
-
-      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
-
-      <button type="submit" disabled={!formValue}>🕊️</button>
-
-    </form>
-  </>)
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder={inputTranslations[props.selectedlanguage]}
+        />
+        <button type="submit" disabled={!formValue}>
+          🕊️
+        </button>
+      </form>
+    </>
+  );
 }
-
 
 function ChatMessage(props) {
-  const { text, uid, photoURL } = props.message;
+  const { text, uid, photoURL, translated } = props.message;
+  const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+  let translatedmessage;
+  if (translated) {
+    translatedmessage = translated[props.selectedlanguage];
+  } else {
+    translatedmessage = "translating...";
+  }
+  console.log("chat message", props);
 
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
-
-  return (<>
-    <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
-      <p>{text}</p>
-    </div>
-  </>)
+  return (
+    <>
+      <div className={`message ${messageClass}`}>
+        <img src={photoURL || "https://placehold.co/600x400"} />
+        <p>{translatedmessage}</p>
+      </div>
+    </>
+  );
 }
-
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
+// import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <GoogleTranslate />
+//       </div>
+//     );
+//   }
+// }
+
+ReactDOM.render(<App />, document.getElementById('root'));
+  test('renders learn react link', () => {
+  act(() =>{const { getByText } = render(<App />);
+  const linkElement = act(() => { getByText(/learn react/i); });
+  });
   expect(linkElement).toBeInTheDocument();
-});
+  });
